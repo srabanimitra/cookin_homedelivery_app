a392170b59bf61e4d7f73d07fe25633688c47ff9
Nusrat Jahan Adiba <nusratadiba06@gmail.com>
2023-12-26 02:21:55 +0600

    Adiba change the main

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text('Amader Flutter Project'),
+        title: Text('Amader'),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
