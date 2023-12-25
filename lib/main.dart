5a34d2590cefeb728cf1275c7d4aa9b0b5310367
prova116 <sumaiyaprova116@gmail.com>
2023-12-26 02:26:21 +0600

    firstbyprova

--- lib/main.dart
+++ lib/main.dart
@@ -25,11 +25,17 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
+
+
   final String title;
+
+
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+
+
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 100;
@@ -89,7 +95,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'prova',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
