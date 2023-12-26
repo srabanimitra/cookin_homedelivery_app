c050cd378540b0844ad2f0dc890cb60a77ddbeb9
Nusrat Jahan Adiba <nusratadiba06@gmail.com>
2023-12-26 13:07:07 +0600

    changed main

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
 
       title: 'Flutter Demo pageeeeeeee',
-      title: 'Flutter Demo app is here',
 
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -21,10 +20,6 @@ class MyApp extends StatelessWidget {
       ),
 
       home: const MyHomePage(title: 'Flutter Demo Home Pageeee'),
-=======
-
-      home: const MyHomePage(title: 'Flutter Demo Home_Page_'),
-
     );
   }
 }
@@ -70,9 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
 
-        title: Text('Amader'),
 
-        title: Text('our app'),
 
       ),
       body: Center(
