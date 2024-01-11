1ff4700771cc1b60fd7de0e003c1bce1197390c0
srabanimitra <srabanimitra2222@gmail.com>
2024-01-11 22:44:53 +0600

    phone

--- lib/view/login/log_in_with_phone.dart
+++ lib/view/login/log_in_with_phone.dart
@@ -1,4 +1,9 @@
+import 'package:cookinapp_01/utils/utils.dart';
+import 'package:cookinapp_01/view/login/otp_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../../common_widget/round_button.dart';
 
 class LoginWithPhone extends StatefulWidget {
   const LoginWithPhone({super.key});
@@ -8,14 +13,51 @@ class LoginWithPhone extends StatefulWidget {
 }
 
 class _LoginWithPhoneState extends State<LoginWithPhone> {
+  bool loading = false;
+
+  final PhoneNumberController = TextEditingController();
+  final auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
       ),
-      body: Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            TextFormField(
+              controller: PhoneNumberController,
+              decoration: InputDecoration(hintText: '+019283983'),
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            RoundButton(
+              title: 'Login',
+              onPressed: () {
+                auth.verifyPhoneNumber(
+                    phoneNumber: PhoneNumberController.text,
+                    verificationCompleted: (_) {},
+                    verificationFailed: (e) {
+                      utils().toastMessage(e.toString());
+                    },
+                    codeSent: (String verificationId, int? token) {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => OtpView()));
+                    },
+                    codeAutoRetrievalTimeout: (e) {
+                      utils().toastMessage(e.toString());
+                    });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
