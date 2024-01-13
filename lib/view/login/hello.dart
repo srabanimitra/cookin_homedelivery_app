d9fbf57d153e60506b4c94c966700726d6c33688
srabanimitra <srabanimitra2222@gmail.com>
2024-01-13 18:04:57 +0600

    google sign in

--- lib/view/login/hello.dart
+++ lib/view/login/hello.dart
@@ -1,32 +1,81 @@
-/*import 'package:firebase_auth/firebase_auth.dart';
+/*import 'package:cookinapp_01/view/login/sign_up_view.dart';
+import 'package:cookinapp_01/view/on_boarding/on_boarding_view.dart';
 import 'package:flutter/material.dart';
+import '../../common/color_extension.dart';
+import '../../common_widget/round_button.dart';
 
-class Hello extends StatefulWidget {
-  const  Hello({super.key});
+import 'welcome_view.dart';
 
+class hello extends SignUpView {
+  const hello({super.key});
   @override
-  State< Hello> createState() => _HelloState();
+  State<hello> createState() => _helloState();
 }
 
-class _HelloState extends State<Hello> {
-  final FirebaseAuth _auth= FirebaseAuth.instance;
+class _helloState extends State<hello> {
   @override
   Widget build(BuildContext context) {
-    String? _email =  _auth.currentUser !.email;
+    var media = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("WELCOME"),
+      body: Column(
+        children: [
+          Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Image.asset("assets/img/brown-nuts-and-pinecone-lot-iphone.jpg",
+                  width: media.width),
+            ],
+          ),
+          SizedBox(
+            height: media.width * 0.1,
+          ),
+          Text(
+            "WELCOME",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: TColor.secondaryText,
+                fontSize: 13,
+                fontWeight: FontWeight.w500),
+          ),
+          SizedBox(
+            height: media.width * 0.1,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: RoundButton(
+              title: "SignOut",
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const WelcomeView(),
+                  ),
+                );
+              },
+            ),
+          ),
+          SizedBox(
+            height: media.width * 0.1,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: RoundButton(
+              title: "Start",
+              type: RoundButtonType.textPrimary,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const OnBoardingView(),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
-      body: const Center(
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(children: [
-            Text("Signed Up With : &_email"),
-          ]),
-          )
-      ),
-    )
-    
+    );
   }
 }
 */
--- lib/view/on_boarding/on_boarding_view.dart
+++ lib/view/on_boarding/on_boarding_view.dart
@@ -3,9 +3,6 @@ import 'package:cookinapp_01/common_widget/round_button.dart';
 import 'package:flutter/material.dart';
 
 import '../main_tabaview/main_tabview.dart';
-//import 'package:food_delivery/common/color_extension.dart';
-//import 'package:food_delivery/common_widget/round_button.dart';
-//import 'package:food_delivery/view/main_tabview/main_tabview.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({super.key});
