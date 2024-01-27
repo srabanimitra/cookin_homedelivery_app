e9307e55eb7cfd0035d48cf66cb8f3f5b66ca0b9
prova116 <sumaiyaprova116@gmail.com>
2024-01-28 01:49:36 +0600

    provachangesnew

--- lib/view/login/welcome_view.dart
+++ lib/view/login/welcome_view.dart
@@ -1,11 +1,14 @@
+
+import 'package:cookinapp_01/view/login/sign_up_view.dart';
 import 'package:flutter/material.dart';
+
 import '../../common/color_extension.dart';
 import '../../common_widget/round_button.dart';
 import 'login_view.dart';
-import 'sign_up_view.dart';
 
 class WelcomeView extends StatefulWidget {
   const WelcomeView({super.key});
+
   @override
   State<WelcomeView> createState() => _WelcomeViewState();
 }
@@ -16,81 +19,70 @@ class _WelcomeViewState extends State<WelcomeView> {
     var media = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              Image.asset("assets/img/welcome_top_shape.png",
-                  width: media.width),
-              Image.asset(
-                "assets/img/app_logo.png",
-                width: media.width * 0.56,
-                height: media.width * 0.54,
-                fit: BoxFit.contain,
+      body: SingleChildScrollView( // Wrap the Column with SingleChildScrollView
+        child: Column(
+          children: [
+            Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Image.asset("assets/img/welcome_top_shape.png",
+                    width: media.width),
+                Image.asset(
+                  "assets/img/app_logo.png",
+                  width: media.width * 0.55,
+                  height: media.width * 0.55,
+                  fit: BoxFit.contain,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: media.width * 0.1,
+            ),
+            Text(
+              "Discover the best foods from over 100\nhomechef and fast delivery to your\ndoorstep",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: TColor.secondaryText,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: media.width * 0.1,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: RoundButton(
+                title: "Login",
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>  LoginView(),
+                    ),
+                  );
+                }, loading: true,
               ),
-            ],
-          ),
-          SizedBox(
-            height: media.width * 0.1,
-          ),
-          Text(
-            "Discover the best foods from over 100\n homechef and fast delivery to your\n doorstep",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: TColor.secondaryText,
-                fontSize: 13,
-                fontWeight: FontWeight.w500),
-          ),
-          SizedBox(
-            height: media.width * 0.1,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: RoundButton(
-              title: "Login",
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const LoginView(),
-                  ),
-                );
-              },
-              loading: true,
             ),
-          ),
-          SizedBox(
-            height: media.width * 0.1,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: RoundButton(
-              title: "SignUp",
-              type: RoundButtonType.textPrimary,
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SignUpView(),
-                  ),
-                );
-              },
-              loading: true,
+            SizedBox(
+              height: media.width * 0.1,
             ),
-          ),
-          /* SizedBox(
-            height: media.width * 0.1,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: RoundButton(
-              title: "SignUp As A Chef",
-              type: RoundButtonType.textPrimary,
-              onPressed: () {},
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: RoundButton(
+                title: "Create an Account",
+                type: RoundButtonType.textPrimary,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SignUpView(),
+                    ),
+                  );
+                }, loading: true,
+              ),
             ),
-          ),*/
-        ],
+          ],
+        ),
       ),
     );
   }
