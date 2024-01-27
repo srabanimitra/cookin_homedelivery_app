b7b7bdacefecbd3ba194d39ea6e529d7d3e3cca8
Nusrat Jahan Adiba <nusratadiba06@gmail.com>
2024-01-27 22:52:37 +0600

    adiba added payment1

--- lib/view/more/notification_view.dart
+++ lib/view/more/notification_view.dart
@@ -1,165 +1,33 @@
 import 'package:cookinapp_01/view/more/my_order_view.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import '../../common/color_extension.dart';
 
 
-class NotificationsView extends StatefulWidget {
-  const NotificationsView({super.key});
+class NotificationsView {
+  //const NotificationsView({super.key});
 
-  @override
-  State<NotificationsView> createState() => _NotificationsViewState();
-}
-class _NotificationsViewState extends State<NotificationsView> {
-  List notificationArr = [
-    {
-      "title": "Your orders has been picked up",
-      "time": "Now",
-    },
-    {
-      "title": "Your order has been delivered",
-      "time": "1 h ago",
-    },
-    {
-      "title": "Your orders has been picked up",
-      "time": "3 h ago",
-    },
-    {
-      "title": "Your order has been delivered",
-      "time": "5 h ago",
-    },
-    {
-      "title": "Your orders has been picked up",
-      "time": "11 Jan 2024",
-    },
-    {
-      "title": "Your order has been delivered",
-      "time": "11 Jan 2024",
-    },
-    {
-      "title": "Your orders has been picked up",
-      "time": "12 Jan 2024",
-    },
-    {
-      "title": "Your order has been delivered",
-      "time": "12 Jan 2024",
-    },
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 46,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: Image.asset("assets/img/btn_back.png",
-                          width: 20, height: 20),
-                    ),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    Expanded(
-                      child: Text(
-                        "Notifications",
-                        style: TextStyle(
-                            color: TColor.primaryText,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w800),
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const MyOrderView()));
-                      },
-                      icon: Image.asset(
-                        "assets/img/shopping_cart.png",
-                        width: 25,
-                        height: 25,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              ListView.separated(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                padding: EdgeInsets.zero,
-                itemCount: notificationArr.length,
-                separatorBuilder: ((context, index) => Divider(
-                  indent: 25,
-                  endIndent: 25,
-                  color: TColor.secondaryText.withOpacity(0.4),
-                  height: 1,
-                )),
-                itemBuilder: ((context, index) {
-                  var cObj = notificationArr[index] as Map? ?? {};
-                  return Container(
-                    decoration: BoxDecoration(color: index % 2 == 0 ? TColor.white : TColor.textfield ),
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 15, horizontal: 25),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          margin: const EdgeInsets.only(top: 4),
-                          width: 8,
-                          height: 8,
-                          decoration: BoxDecoration(
-                              color: TColor.primary,
-                              borderRadius: BorderRadius.circular(4)),
-                        ),
-                        const SizedBox(
-                          width: 15,
-                        ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                cObj["title"].toString(),
-                                style: TextStyle(
-                                    color: TColor.primaryText,
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w600),
-                              ),
-                              const SizedBox(
-                                height: 4,
-                              ),
-                              Text(
-                                cObj["time"].toString(),
-                                style: TextStyle(
-                                    color: TColor.secondaryText,
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w500),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                }),
-              ),
-            ],
-          ),
-        ),
-      ),
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  void requestNotificationPermission()async{
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: true,
+      badge: true,
+      carPlay: true,
+      criticalAlert: true,
+      provisional: true,
+      sound: true
+
     );
+    if(settings.authorizationStatus == AuthorizationStatus.authorized){
+      print("user granted permission");
+    }else if(settings.authorizationStatus == AuthorizationStatus.provisional){
+      print("user granted provisional permission");
+    }else{
+      print("user denied permission");
+    }
   }
+
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,8 @@ import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_database
+import firebase_messaging
+import flutter_local_notifications
 import google_sign_in_ios
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -18,5 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
 }
